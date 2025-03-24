4bfd08512010e2dfd3fce7dac3c0ab932ffe7e60
Mohanraaji B <mmohanraaji@griddynamics.com>
2025-03-24 17:18:30 +0530

    JSON read and write File references

---
+++ JSON_Practise_2/Person.cs
@@ -0,0 +1,9 @@
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Address { get; set; }
+    public string Gender { get; set; }
+}
---
+++ JSON_Practise_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Text.Json;
+
+class Program
+{
+    public static string filePath =
+        "/Users/mmohanraaji/Documents/E/Practise Programs/JSON_Practise_2/JSON_Practise_2/JSON_Practise_2/person.json";
+    
+    static void Main()
+    {
+        ReadFile();
+        WriteFile();
+        ReadFile();
+    }
+
+    public static void WriteFile()
+    {
+        
+        Person person = new Person
+        {
+            FirstName = "Mohan",
+            LastName = "Raji",
+            Address = "123 Main Street",
+            Gender = "Male",
+            Age = 22,
+        };
+
+        string JsonString = JsonSerializer.Serialize(person);
+        File.WriteAllText(filePath, JsonString);
+    }
+
+    public static void ReadFile()
+    {
+        string JsonString = File.ReadAllText(filePath);
+        Person person = JsonSerializer.Deserialize<Person>(JsonString);
+        Console.WriteLine($"The name is {person.FirstName} {person.LastName} who is a {person.Gender} of age {person.Age} lives in {person.Address}");
+    }
+}
